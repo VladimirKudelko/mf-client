b543ee2bfe027d3d8c4f2b5ba051944b4cc3cdf9
UladzimirKudzelka <uladzimir.kudzelka@itechart-group.com>
2020-02-16 19:49:50 +0300

    minor refactoring; add logic for budgets

--- src/app/modules/dashboard/components/budget/budget.component.ts
+++ src/app/modules/dashboard/components/budget/budget.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { MatDialog } from '@angular/material';
 import { ActivatedRoute } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { filter, pluck } from 'rxjs/operators';
 
 import * as moment from 'moment';
 
-import { SidebarService, BudgetService, TransactionService } from 'src/app/shared/services';
+import { SidebarService, BudgetService, TransactionService, AuthService } from 'src/app/shared/services';
 import { AddBudgetModalComponent, NotificationModalComponent } from 'src/app/shared/components/modals';
 import { User, Budget } from 'src/app/shared/models';
 import { PopupEnum } from 'src/app/shared/enums';
@@ -22,7 +22,10 @@ export class BudgetComponent implements OnInit {
   public user: User;
   public allExpenses: Budget;
   public leftMoney: number;
-  public progressBarValue: number;
+
+  public get allExpensesPercentage(): number {
+    return (this.leftMoney * 100) / this.allExpenses.limit;
+  }
 
   constructor(
     private _dialog: MatDialog,
@@ -30,7 +33,8 @@ export class BudgetComponent implements OnInit {
     private _route: ActivatedRoute,
     private _sidebarService: SidebarService,
     private _budgetService: BudgetService,
-    private _transactionService: TransactionService
+    private _transactionService: TransactionService,
+    private _authService: AuthService
   ) { }
 
   ngOnInit(): void {
@@ -38,20 +42,41 @@ export class BudgetComponent implements OnInit {
 
     this.user = this._route.snapshot.data.user;
 
+    this.updateBudget();
+  }
+
+  private getUser(): void {
+    this._authService
+      .getUserById()
+      .pipe(
+        filter(response => !!response && !!response.user),
+        pluck('user')
+      )
+      .subscribe((user: User) => {
+        this.user = user;
+
+        this.updateBudget();
+        this._cdr.detectChanges();
+      });
+  }
+
+  private updateBudget(): void {
     if (this.user.budget && this.user.budget.allExpenses) {
       this.allExpenses = this.user.budget.allExpenses;
-
-      this._transactionService
-        .getUserExpensesByPeriod(
-          moment(this.allExpenses.from).format('YYYY-MM-DD'),
-          moment(this.allExpenses.to).format('YYYY-MM-DD')
-        )
-        .subscribe(response => {
-          this.leftMoney = response.left;
-          this.progressBarValue = (this.leftMoney * 100) / this.allExpenses.limit;
-          this._cdr.detectChanges();
-        });
+      this.getUserTransactions();
     }
+  }
+
+  private getUserTransactions(): void {
+    this._transactionService
+      .getUserExpensesByPeriod(
+        moment(this.allExpenses.from).format('YYYY-MM-DD'),
+        moment(this.allExpenses.to).format('YYYY-MM-DD')
+      )
+      .subscribe(response => {
+        this.leftMoney = response.left;
+        this._cdr.detectChanges();
+      });
   }
 
   private showNotificationModal(modalType: PopupEnum, message: string): void {
@@ -72,6 +97,9 @@ export class BudgetComponent implements OnInit {
   public addBudget(budget: any): void {
     this._budgetService
       .addBudget({ ...budget, currency: this.user.currency })
-      .subscribe(() => this.showNotificationModal(PopupEnum.Success, 'Budget has been successfully added'));
+      .subscribe(() => {
+        this.getUser();
+        this.showNotificationModal(PopupEnum.Success, 'Budget has been successfully added');
+      });
   }
 }
